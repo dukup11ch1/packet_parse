36a7d0ad278f04b64fa6e4eed27aeba4258a6a21
dukup11ch1 <dukup11ch1@gmail.com>
2018-08-20 06:06:07 +0900

    bin update

--- packet_filter/packet_filter.py
+++ packet_filter/packet_filter.py
@@ -1,24 +1,24 @@
 import socket
 import sys
 import header
+import pydivert
 
-try:
-    s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_IP)
-    s.bind((sys.argv[1],0))
-    s.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
-    s.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
-except socket.error , msg:
-    print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+w = pydivert.WinDivert()
+w.open()
 
 while True:
-    packet = s.recvfrom(65565)
-    packet = packet[0]
+    
+    packet,recv_len,address = w.recv()
+    #print packet
     head = header.Tcp(packet)
     if head.dstport()==80:
         try:
             if head.data().split()[4]==sys.argv[2]:
-                print "block"
+                print "block!"
+                continue
         
         except:
             pass
+
+    w.send(packet,recv_len,address)
+w.close()
